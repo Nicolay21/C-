7b6d7c95bf6e066bef9bde77f6b55f56f619d509
Nicolay21 <78875139+Nicolay21@users.noreply.github.com>
2021-02-15 13:13:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using myLibrary;    // подключение пространства имен из библиотеки классов
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square_m square = new Square_m ();
+
+            square.Circle(5);
+            square.Triangle(5,5,5,0);
+
+        }
+    }
+}
---
+++ Square_m.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace myLibrary
+{
+    public class Square_m
+    {
+        public void Circle(double rad) // площадь круга
+        {
+            double pi = Math.PI;
+            double squ;
+
+            squ = pi * rad * rad;
+            
+            Console.WriteLine("Площадь круга =") ;
+            Console.WriteLine(squ);
+        }
+        public void Triangle(double a_t, double b_t, double c_t, int au = 1) // площадь треугольника
+        {
+            double per_2 = (a_t + b_t + c_t) / 2;
+
+            double squ = Math.Sqrt(per_2 * (per_2 - a_t) * (per_2 - b_t) * (per_2 - c_t));
+
+            Console.WriteLine("Площадь треугольника =");
+            Console.WriteLine(squ);
+
+            if (au == 0)
+            {
+
+                if (a_t > b_t && a_t > c_t)
+                {
+                    double num_1 = a_t * a_t;
+                    double num_2 = b_t * b_t + c_t * c_t;
+                    if (num_1 == num_2)
+                    {
+                        Console.WriteLine("Прямоугольный");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Не прямоугольный");
+                    }
+                }
+                else if (b_t > a_t && b_t > c_t)
+                {
+                    double num_1 = b_t * b_t;
+                    double num_2 = a_t * a_t + c_t * c_t;
+                    if (num_1 == num_2)
+                    {
+                        Console.WriteLine("Прямоугольный");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Не прямоугольный");
+                    }
+                }
+                else if (c_t > a_t && c_t > b_t)
+                {
+                    double num_1 = c_t * c_t;
+                    double num_2 = a_t * a_t + b_t * b_t;
+                    if (num_1 == num_2)
+                    {
+                        Console.WriteLine("Прямоугольный");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Не прямоугольный");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(" Не прямоугольный");
+                }
+            }
+        }
+    }
+}
+
+// запрос SQL 
+// SELECT `autor`,`book` FROM `values` JOIN `books` ON (values.id_book = books.id_book) JOIN `autors` ON (values.id_autor = autors.id_autor)
